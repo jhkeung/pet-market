365c578994dada9d37c28db18a76c28e6e957e33
Jocelyn Keung <jocelyn.keung@captechventures.com>
2017-11-02 17:42:42 -0400

    Adding application, header, landing page, pet detail components; importing bootstrap and font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PetDetailComponent } from './pet-detail/pet-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ApplicationComponent } from './application/application.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetDetailComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    ApplicationComponent
   ],
   imports: [
     BrowserModule,
