b5eb10b6666638dc4b9cef6aaf8992e7bc4f0f58
Jocelyn Keung <jocelyn.keung@captechventures.com>
2017-11-03 16:02:20 -0400

    Adding routing for browse, landing page, and application views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule} from '@angular/http';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PetDetailComponent } from './pet-detail/pet-detail.component';
@@ -9,7 +9,24 @@ import { HeaderComponent } from './header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ApplicationComponent } from './application/application.component';
 import { PetSearchService } from './service/pet-search.service';
+import { RouterModule, Routes } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap';
+
+const appRoutes: Routes = [
+  {
+    path: 'apply',
+    component: ApplicationComponent
+  },
+  {
+    path: 'browse',
+    component: PetDetailComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LandingPageComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -23,9 +40,10 @@ import { BsDropdownModule } from 'ngx-bootstrap';
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
     BsDropdownModule.forRoot()
   ],
   providers: [PetSearchService],
   bootstrap: [AppComponent]
-  })
+})
 export class AppModule { }
